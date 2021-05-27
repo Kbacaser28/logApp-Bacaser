6b3143c4d170811f80e552be960451bde6cab873
KenethBacaser <201880038@psu.palawan.edu.ph>
2021-05-27 23:23:17 +0800

    Practice-10-Done

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 	define('ROOT_URL', 'http://localhost/logApp-scaling-octo/');
-	define('DB_HOST', 'localhost');
-	define('DB_USER', 'root');
-	define('DB_PASS', '1234567');
-	define('DB_NAME', 'logapp');
+	define('DB_HOST', 'sql6.freesqldatabase.com');
+	define('DB_USER', 'sql6415307');
+	define('DB_PASS', 'QM56gs8HpG');
+	define('DB_NAME', 'sql6415307');
